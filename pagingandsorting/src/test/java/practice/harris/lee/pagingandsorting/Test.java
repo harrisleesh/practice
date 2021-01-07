9f726e9235ce80daf6d227e3cb05a1a6b793e28e
이성훈 <harrisleesh@gmail.com>
2021-01-08 06:35:48 +0900

    Test Collections API

---
+++ pagingandsorting/src/test/java/practice/harris/lee/pagingandsorting/Test.java
@@ -0,0 +1,11 @@
+package practice.harris.lee.pagingandsorting;
+
+import java.util.*;
+
+public class Test {
+    List<Integer> test = new ArrayList<>();
+    Queue<Integer> testQUeu = new LinkedList<>();
+    Stack<Integer> stack;
+
+    Integer[] arr = {1, 2, 3};
+}
